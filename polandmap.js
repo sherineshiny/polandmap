8ca15f8e2a47c513956e0be33b3e1969e9c1bf36
sherineshiny <73679968+sherineshiny@users.noreply.github.com>
2022-03-25 23:23:38 +0530

    Update polandmap.js

--- polandmap.js
+++ polandmap.js
@@ -1,10 +1,6 @@
-function getMyImageToolTip(element,messageText) {
-var input = document.querySelector(element),
-tooltip = input.children[0];
-main.children[0] main.addEventListener('mouseover',function() {
-tooltip.innerHTML = messageText;
-})
-}
+$(document).ready(function() {
+  $('.tooltip').tooltipster({});
+});
 getMyImageToolTip('.westpomerania');
 getMyImageToolTip('.lubusz');
 getMyImageToolTip('.lowersilesia');
