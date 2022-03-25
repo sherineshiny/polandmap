33c6c35d729091d2cf42b80501869f331e7f352f
sherineshiny <73679968+sherineshiny@users.noreply.github.com>
2022-03-25 23:52:10 +0530

    Update polandmap.js

--- polandmap.js
+++ polandmap.js
@@ -1,21 +1,7 @@
-$(document).ready(function() {
-  $('.tooltip').tooltipster({});
+$(function() {
+    $('a.popper').hover(function() {
+        $('#pop').toggle();
+    });
 });
-getMyImageToolTip('.westpomerania');
-getMyImageToolTip('.lubusz');
-getMyImageToolTip('.lowersilesia');
-getMyImageToolTip('.greaterpoland');
-getMyImageToolTip('.kuyavia');
-getMyImageToolTip('.pomerania');
-getMyImageToolTip('.warmia');
-getMyImageToolTip('.podlaskie');
-getMyImageToolTip('.masovia');
-getMyImageToolTip('.lodz');
-getMyImageToolTip('.lublin');
-getMyImageToolTip('.opole');
-getMyImageToolTip('.silesia');
-getMyImageToolTip('.holycrossprovince');
-getMyImageToolTip('.lesserpoland');
-getMyImageToolTip('.subcarpathia');
 
 
