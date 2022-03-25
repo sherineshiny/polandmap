5be65f7a1a4f7376911c61011b30b7f79677c592
sherineshiny <73679968+sherineshiny@users.noreply.github.com>
2022-03-25 22:14:55 +0530

    Create polandmap.js

---
+++ polandmap.js
@@ -0,0 +1,25 @@
+function getMyImageToolTip(element,messageText) {
+var input = document.querySelector(element),
+tooltip = input.children[0];
+main.children[0] main.addEventListener('mouseover',function() {
+tooltip.innerHTML = messageText;
+})
+}
+getMyImageToolTip('.westpomerania');
+getMyImageToolTip('.lubusz');
+getMyImageToolTip('.lowersilesia');
+getMyImageToolTip('.greaterpoland');
+getMyImageToolTip('.kuyavia');
+getMyImageToolTip('.pomerania');
+getMyImageToolTip('.warmia');
+getMyImageToolTip('.podlaskie');
+getMyImageToolTip('.masovia');
+getMyImageToolTip('.lodz');
+getMyImageToolTip('.lublin');
+getMyImageToolTip('.opole');
+getMyImageToolTip('.silesia');
+getMyImageToolTip('.holycrossprovince');
+getMyImageToolTip('.lesserpoland');
+getMyImageToolTip('.subcarpathia');
+
+
